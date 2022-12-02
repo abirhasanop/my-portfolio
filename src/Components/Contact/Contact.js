6e1e2de005615129ac480c153e1e6be2c7ede900
abirhasan <abir77077@gmail.com>
2022-12-02 19:14:17 +0600

    services section added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,7 +6,16 @@ const Contact = () => {
             <h5>Get In Touch</h5>
             <h2>Contact Me</h2>
 
-            
+            <div className="container contact__container">
+                {/* options */}
+                <div className="contact__option"></div>
+
+
+                {/* contact form */}
+                <form>
+                    
+                </form>
+            </div>
         </section>
     );
 };
